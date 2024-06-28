2bcbca6c231c475fa408d8d707d5787c3756ac93
Shailesh Kaliya <117556933+shaileshkaliya@users.noreply.github.com>
2024-06-28 11:37:07 +0530

    Time: 4 ms (99.46%), Space: 63.4 MB (41.62%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -1,27 +1,21 @@
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        int[][] arr = new int[n][2];
-        for(int i=0;i<n;i++) arr[i][0] = i;
-        for(int[] road:roads){
-            arr[road[0]][1]++;
-            arr[road[1]][1]++;
+        int[] br = new int[n];
+        for(int[] r : roads){
+            br[r[0]]++;
+            br[r[1]]++;
         }
-        Arrays.sort(arr, new Comparator<int[]>(){
-            public int compare(int[]a1, int[] a2){
-                return Integer.compare(a2[1], a1[1]);
+        int[] cnt = new int[n];
+        for(int b : br){
+            cnt[b]++;
+        }
+        long sum = 0;
+        long val = 1;
+        for(long i = 0; i < n; i++){
+            for(int j = 0; j < cnt[(int)i]; j++){
+                sum += i*val++;
             }
-        });
-        int[] val = new int[n];
-        int t = n;
-        for(int i=0;i<n;i++){
-            val[arr[i][0]] = t;
-            t--;
         }
-        long ans=0;
-        for(int[] road:roads){
-            long temp = val[road[0]]+val[road[1]];
-            ans+=temp;
-        }
-        return ans;
+        return sum;
     }
 }
