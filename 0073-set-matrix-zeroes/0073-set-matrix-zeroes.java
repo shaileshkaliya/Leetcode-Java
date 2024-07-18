5562d8a6186b32ce382c2cd732905e60bd8e3cf7
Shailesh Kaliya <117556933+shaileshkaliya@users.noreply.github.com>
2024-07-18 08:36:25 +0530

    Time: 0 ms (100%), Space: 45.4 MB (55.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,23 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> hor = new HashSet<>();
-        Set<Integer> ver = new HashSet<>();
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0){
-                    hor.add(i);
-                    ver.add(j);
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] hor = new boolean[m];
+        boolean[] ver = new boolean[n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 0){
+                    hor[i]=true;
+                    ver[j]=true;
                 }
             }
         }
 
-        for(int j:hor){
-            for(int i=0;i<matrix[0].length;i++){
-                matrix[j][i]=0;
-            }
-        }
-        for(int j:ver){
-            for(int i=0;i<matrix.length;i++){
-                matrix[i][j]=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(hor[i] || ver[j]){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
